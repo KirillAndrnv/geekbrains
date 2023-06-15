dbf3e33e77f263a898b6f437e5eff620cf3f636a
KirillAndrnv <rafukqwer@mail.ru>
2023-06-15 12:26:49 +0700

    Zadacha7LegcheCHemYaDymal

---
+++ git education/C++ practice/SemOnePractiseInGroup/secondTask/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+Console.Write("Write number:");
+
+int a = int.Parse(Console.ReadLine());
+
+
+if (a == 1)
+{  
+    Console.Write("sunday");
+}
+if (a == 2)
+{  
+    Console.Write("monday");
+}
+if (a == 3)
+{  
+    Console.Write("tuesday");
+}
+if (a == 4)
+{  
+    Console.Write("wednesday");
+}
+if (a == 5)
+{  
+    Console.Write("thursday");
+}
+if (a == 6)
+{  
+    Console.Write("friday");
+}
+if (a == 7)
+{  
+    Console.Write("sunday");
+}
+else
+{
+    Console.Write("Dragons live in this lands");
+}
---
+++ git education/C++ practice/SemOnePractiseInGroup/taskThidr/Program.cs
@@ -0,0 +1,15 @@
+﻿// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+Console.Clear();
+Console.Write("Write number N:");
+
+int N = int.Parse(Console.ReadLine());
+ 
+int a = -N;
+
+while (a <= N)
+{
+    Console.Write($"{a}, ");
+    a++;
+}
+
