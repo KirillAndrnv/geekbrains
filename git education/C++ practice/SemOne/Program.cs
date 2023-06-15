b8a5788896e054fa4586f8b850dd80f051a87492
KirillAndrnv <rafukqwer@mail.ru>
2023-06-15 11:28:19 +0700

    zadachaZarabotalo

---
+++ git education/C++ practice/SemOne/Program.cs
@@ -0,0 +1,9 @@
+﻿//Hz than task is it, but it work and im happy ^_^
+Console.Write("Enter the number a:");
+int a = int.Parse(Console.ReadLine());
+//First method 
+int result1 = a * a;
+Console.WriteLine($"First method -> Number {a} Square is {result1}");
+//Second method 
+int result2 = Convert.ToInt32(Math.Pow(a, 2));
+Console.WriteLine($"Second Method  -> square of {a} is {result1}");
---
+++ git education/C++ practice/SemOnePractiseInGroup/Program.cs
@@ -0,0 +1,30 @@
+﻿// a = 25; b = 5->да
+// a = 2 b = 10->нет
+// a = 9; b = -3->да
+// a = -3 b = 9->нет
+
+
+Console.Clear();
+Console.Write("Enter the number a:");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the number b:");
+
+int b = int.Parse(Console.ReadLine());
+
+
+int sqr = a * a;
+
+if (sqr == b)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+Console.WriteLine($"Square {a} Is {sqr}");
+
+
